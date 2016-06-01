1921ae1fef05d7a9e7fd79580103c178dfb9328a
Testla <yeahemc2@yeah.net>
2016-06-01 21:52:59 +0800

    added the simplest controller of login_and_register.html

--- SAAD-final/src/main/java/hello/Application.java
+++ SAAD-final/src/main/java/hello/Application.java
@@ -1,28 +1,17 @@
 package hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application {
 
-	@Autowired
-	private CustomerService customerService;
+//	@Autowired
+//	private CustomerService customerService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		// initialize
-		customerService.init();
-
-		System.out.println("zgw exists: " + (Boolean)customerService.exists("zgw"));
-
 	}
 
 }
